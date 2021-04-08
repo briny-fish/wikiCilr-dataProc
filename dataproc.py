318da253cd799804a2fee5e2196b41e60815233a
briny-fish <1179102664@qq.com>
2021-04-08 16:15:18 +0800

    update:wq

--- dataproc.py
+++ dataproc.py
@@ -1,7 +1,7 @@
 from xml.etree.ElementTree import iterparse
 from lxml import etree
 import re
-outf = open('out.txt', 'w',encoding='utf-8')
+outf = open('out600.txt', 'w',encoding='utf-8')
 context = etree.iterparse('F:\data\enwiki-20210101-pages-articles-multistream.xml')
 #context = iter(context)
 cnt = 0
@@ -33,9 +33,17 @@ for event, elem in context:
         #print(title)
     if (elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}text'):
         text = elem.text
-        if(len(text)<3):continue
-        loc = text.find("'''")
-        #print(loc)
+        if (text==None or len(text) < 3): continue
+        text = re.sub('\n', 'SSSSSS', text)
+        text = re.sub(r"{{Infobox",'EEEEEE',text)
+        #text = re.sub(r"EEEEEE(.*)SSSSSS\|",' ',text)
+
+        loc = re.search(r"(SSSSSS)(.{0,10})'''",text)
+        if(loc != None):loc = loc.span()[0]
+        else: loc = -1
+        #if id!='' and int(id) == 595:
+        #    print(text)
+         #   break
         if(loc!=-1):
             loceq = -1
             for a in re.finditer('==', text):
@@ -43,8 +51,18 @@ for event, elem in context:
                     loceq = a.span()[0]
                     break
             print(id)
-            st = ''.join([id,'\t',title,'\t',re.sub('\n','',text[loc:loceq])])
-            #outf.write(st)
+            text = text[loc:loceq]
+
+            text = re.sub(r"'''|''|\t",'',text)
+            text = re.sub(r'<ref(.*?)(</ref>|/>)', ' ', text)
+            text = re.sub(r'{{(.*?)({{(.*?)({{(.*?)}}?)(.*?)}}?)(.*?)}}', '', text)
+            text = re.sub(r'{{(.*?)}}','',text)
+            text = re.sub(r'\[\[(?:[^|\]]*\|)?([^\]]*)]]', r'\1', text)
+
+            text = re.sub(r'SSSSSS|EEEEEE','',text)
+            #print(re.findall(r'<ref(.*?)</ref>|/>',text))
+            st = ''.join([id,'\t',title,'\t',text[:1000],'\n'])
+            outf.write(st)
     elem.clear()
     # 选取当前节点的所有先辈（父、祖父等）以及当前节点本身
     for ancestor in elem.xpath('ancestor-or-self::*'):
